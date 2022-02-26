b7045523d6c14cb9c124e87714c45573057863d2
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-26 21:43:05 +0900

    e2e testing : patch and bad post

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,12 +34,25 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'test Movie', year: 2022, genres: ['test'] })
         .expect(201);
     });
+
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'test Movie',
+          year: 2022,
+          genres: ['test'],
+          other: 12345,
+        })
+        .expect(400);
+    });
+
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
@@ -49,7 +62,16 @@ describe('AppController (e2e)', () => {
     it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'updated test' })
+        .expect(200);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
